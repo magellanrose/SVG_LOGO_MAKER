914dfc2550528434552601efd6929e7a51d65c2e
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-17 19:29:05 -0500

    added txt and color to circle

--- generateLogo.js
+++ generateLogo.js
@@ -3,6 +3,7 @@ class Circle {
         return `
         <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
         <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="${data.color}" />
+        <text x="50" y="50" font-size="20" text-anchor="middle" alignment-baseline="middle" fill="${data.character_color}">${data.characters}</text>
       </svg>
 `
     }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateLogo = require('./generateLogo')
 
+// QUESTIONS THAT WILL BE PROMPTED INTO THE TERMINAL
 const questions = [
   {
     type: 'list',
